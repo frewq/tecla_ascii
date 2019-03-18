c716c62a2c8c461a8cffc34bebf5894478f8e26d
frewq <fabi85_592@msn.com>
2019-03-18 00:46:41 -0300

    v0.2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-document.body.addEventListener('keypress', 
-  (tecla) => (
-    document.querySelector('#tecla').innerHTML = `Tecla: <span id='colortecla'>${tecla.key}</span>`,
-    document.querySelector('#codigo').innerHTML = `Código ASCII: <span id='colortecla'>${tecla.keyCode}</span>`
-  )
-);
+document.body.addEventListener('keypress', (tecla) => presionoTecla(tecla));
+document.body.addEventListener('keydown', (tecla) => presionoTecla(tecla));
+    
+function presionoTecla(tecla){
+  document.querySelector('#colortecla').textContent = `${tecla.key}`,
+  document.querySelector('#colorcodigo').textContent = `${tecla.keyCode}`
+}
